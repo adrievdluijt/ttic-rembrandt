cc442e04ed7aa1b518391544c40244db112cd149
Adrie van der Luijt <adrie@adrie.co.uk>
2026-05-09 21:00:24 +0100

    Update review.js

--- api/review.js
+++ api/review.js
@@ -190,7 +190,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         model: MODEL,
-        max_tokens: 4000,
+        max_tokens: 8192,
         system: buildSystemPrompt(jurisdiction, safeContext),
         messages: [{
           role: 'user',
