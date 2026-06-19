53b086df8482ac8e383b3136bededfcd5a80c590
Adrie van der Luijt <adrie@adrie.co.uk>
2026-06-19 18:56:33 +0100

    Update feedback.js

--- api/feedback.js
+++ api/feedback.js
@@ -41,7 +41,7 @@ function applyCors(req, res) {
     res.setHeader('Vary', 'Origin');
   }
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 }
 
 export default async function handler(req, res) {
